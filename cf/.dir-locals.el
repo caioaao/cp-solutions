da6e5dbc4e8ec4793c16861e81db36c32af4f36a
Caio Oliveira <caioaao@gmail.com>
2020-07-03 18:36:20 -0400

    default repl for ocaml in codeforces

--- cf/.dir-locals.el
+++ cf/.dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((tuareg-mode
-  (compile-command . "opam exec --switch=4.02.1-codeforces -- ocamlfind ocamlopt nums.cmxa str.cmxa -pp camlp4o -unsafe sol.ml -o sol.exe-ocaml && ./sol.exe-ocaml < in.txt | tee out.txt")))
+  (compile-command . "opam exec --switch=4.02.1-codeforces -- ocamlfind ocamlopt nums.cmxa str.cmxa -pp camlp4o -unsafe sol.ml -o sol.exe-ocaml && ./sol.exe-ocaml < in.txt | tee out.txt")
+  (tuareg-interactive-program . "opam exec --switch=4.02.1-codeforces -- ocaml -nopromptcont")))
